5bedd4103b1bf31e1be4901b7843f84ed49c2ed4
Makary Hryshkin <mhryshkin@gmail.com>
2023-07-24 11:03:54 +0200

    Added missing tailwind config styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./index.html', './src/**/*.{ts,tsx}'],
-    theme: {
-        extend: {},
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      animation: {
+        'fade-out': 'fade-out 2s forwards 1s',
+      },
+      keyframes: {
+        'fade-out': {
+          from: { opacity: 1 },
+          to: { opacity: 0 },
+        },
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
